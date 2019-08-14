5188e9122c7d4093e4bfb3d010c3322ec7c723dd
Brian Steventh Torres Yañez <xdbrian.steventh@gmail.com>
2019-08-13 23:49:00 -0500

    delete file

--- src/main/java/com/example/demo/UsuarioController.java
+++ src/main/java/com/example/demo/UsuarioController.java
@@ -1,4 +1,4 @@
-package com.example.demo.codeExamples.usuario;
+package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
